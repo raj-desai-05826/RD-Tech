9a0a7771e0a6e0ec40ababc2542c2c5048a795c7
Raj Desai <146960162+raj-desai-05826@users.noreply.github.com>
2024-04-06 20:45:20 +0530

    upload all files

---
+++ js/welcome.js
@@ -0,0 +1,18 @@
+let stu = document.getElementById("stu-btn");
+let admin = document.getElementById("admin-btn");
+let nonbtn = document.getElementById("non-btn");
+
+stu.onclick = () =>{
+    alert("R🥰D");
+    alert("Moye Moye");
+}
+
+admin.onclick = () =>{
+    alert("R🥰D");
+    alert("Moye Moye");
+}
+
+nonbtn.onclick = () =>{
+    alert("R🥰D");
+    alert("Moye Moye");
+}
